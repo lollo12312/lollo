7492afa70a69287d5f23b819ebbe843d29e49106
lollo <email@example.com>
2025-04-26 22:50:12 +0800

    黄家洛

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SysPage from './SysPage';
+import MyTitle from './SysPage/MyTitle';
+import SysPage2 from './SysPage2';
+import Navig from './SysPage2/Navig';
+
+const App = () => {
+
+  return (
+    <>
+      {/* <MyTitle></MyTitle> */}
+      <SysPage2></SysPage2>
+    </>
+  );
+}
+
+export default App;
+
+
